a42566ee66b0785ac1aa24e6dde683a611e050dc
karenzhao35 <karenzhao35@gmail.com>
2025-01-18 14:28:15 -0800

    swift frontend

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Karen Zhao on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/frontendTests/frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  frontendTests.swift
+//  frontendTests
+//
+//  Created by Karen Zhao on 2025-01-18.
+//
+
+import Testing
+@testable import frontend
+
+struct frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
